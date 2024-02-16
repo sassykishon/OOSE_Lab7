6157403961b95a3e24c275745355a352e682a602
DerekSomervilleUofG <78212219+DerekSomervilleUofG@users.noreply.github.com>
2024-02-16 11:17:13 +0000

    straight suit

--- src/test/java/PreValidation/HandTestExist.java
+++ src/test/java/PreValidation/HandTestExist.java
@@ -23,49 +23,10 @@ public class HandTestExist{
     assertTrue(javaFile.hasMethodByName("getLastCard"));
   }
 
-  @Test
-  void getHandOfCardsExist(){
-    assertTrue(javaFile.hasMethodByName("getHandOfCards"));
-  }
 
-  @Test
-  void playACardNoParamExist(){
-    assertTrue(javaFile.hasMethodByName("playACardNoParam"));
-  }
 
-  @Test
-  void playACardIntegerExist(){
-    assertTrue(javaFile.hasMethodByName("playACardInteger"));
-  }
+  
 
-  @Test
-  void copyExist(){
-    assertTrue(javaFile.hasMethodByName("copy"));
-  }
-
-  @Test
-  void copySizeExist(){
-    assertTrue(javaFile.hasMethodByName("copySize"));
-  }
-
-  @Test
-  void isEmptyExist(){
-    assertTrue(javaFile.hasMethodByName("isEmpty"));
-  }
-
-  @Test
-  void sizeExist(){
-    assertTrue(javaFile.hasMethodByName("size"));
-  }
-
-  @Test
-  void sizeZeroExist(){
-    assertTrue(javaFile.hasMethodByName("sizeZero"));
-  }
-
-  @Test
-  void findACardExist(){
-    assertTrue(javaFile.hasMethodByName("findACard"));
-  }
+  
 
 }
